52c44e64ed7dbf3cfc13476ecfd34782fb71a428
Daniel Souza <dassilved@gmail.com>
2024-10-10 19:14:57 -0300

    sql files fixed. pbi file added

--- powerbi tasks/azure database/insercao_de_dados_e_queries_sql.sql
+++ powerbi tasks/azure database/insercao_de_dados_e_queries_sql.sql
@@ -1,65 +1,75 @@
 --File provided by Juliana Zanelatto
 --https://github.com/julianazanelatto/power_bi_analyst/
 
-use company_constraints;
+use azure_company;
 
 insert into employee values ('John', 'B', 'Smith', 123456789, '1965-01-09', '731-Fondren-Houston-TX', 'M', 30000, 333445555, 5),
-							('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
-                            ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321-Castle-Spring-TX', 'F', 25000, 987654321, 4),
-                            ('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291-Berry-Bellaire-TX', 'F', 43000, 888665555, 4),
-                            ('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975-Fire-Oak-Humble-TX', 'M', 38000, 333445555, 5),
-                            ('Joyce', 'A', 'English', 453453453, '1972-07-31', '5631-Rice-Houston-TX', 'F', 25000, 333445555, 5),
-                            ('Ahmad', 'V', 'Jabbar', 987987987, '1969-03-29', '980-Dallas-Houston-TX', 'M', 25000, 987654321, 4),
-                            ('James', 'E', 'Borg', 888665555, '1937-11-10', '450-Stone-Houston-TX', 'M', 55000, NULL, 1);
+    ('Franklin', 'T', 'Wong', 333445555, '1955-12-08', '638-Voss-Houston-TX', 'M', 40000, 888665555, 5),
+    ('Alicia', 'J', 'Zelaya', 999887777, '1968-01-19', '3321-Castle-Spring-TX', 'F', 25000, 987654321, 4),
+    ('Jennifer', 'S', 'Wallace', 987654321, '1941-06-20', '291-Berry-Bellaire-TX', 'F', 43000, 888665555, 4),
+    ('Ramesh', 'K', 'Narayan', 666884444, '1962-09-15', '975-Fire-Oak-Humble-TX', 'M', 38000, 333445555, 5),
+    ('Joyce', 'A', 'English', 453453453, '1972-07-31', '5631-Rice-Houston-TX', 'F', 25000, 333445555, 5),
+    ('Ahmad', 'V', 'Jabbar', 987987987, '1969-03-29', '980-Dallas-Houston-TX', 'M', 25000, 987654321, 4),
+    ('James', 'E', 'Borg', 888665555, '1937-11-10', '450-Stone-Houston-TX', 'M', 55000, NULL, 1);
 
 insert into dependent values (333445555, 'Alice', 'F', '1986-04-05', 'Daughter'),
-							 (333445555, 'Theodore', 'M', '1983-10-25', 'Son'),
-                             (333445555, 'Joy', 'F', '1958-05-03', 'Spouse'),
-                             (987654321, 'Abner', 'M', '1942-02-28', 'Spouse'),
-                             (123456789, 'Michael', 'M', '1988-01-04', 'Son'),
-                             (123456789, 'Alice', 'F', '1988-12-30', 'Daughter'),
-                             (123456789, 'Elizabeth', 'F', '1967-05-05', 'Spouse');
+    (333445555, 'Theodore', 'M', '1983-10-25', 'Son'),
+    (333445555, 'Joy', 'F', '1958-05-03', 'Spouse'),
+    (987654321, 'Abner', 'M', '1942-02-28', 'Spouse'),
+    (123456789, 'Michael', 'M', '1988-01-04', 'Son'),
+    (123456789, 'Alice', 'F', '1988-12-30', 'Daughter'),
+    (123456789, 'Elizabeth', 'F', '1967-05-05', 'Spouse');
 
 insert into departament values ('Research', 5, 333445555, '1988-05-22','1986-05-22'),
-							   ('Administration', 4, 987654321, '1995-01-01','1994-01-01'),
-                               ('Headquarters', 1, 888665555,'1981-06-19','1980-06-19');
+    ('Administration', 4, 987654321, '1995-01-01','1994-01-01'),
+    ('Headquarters', 1, 888665555,'1981-06-19','1980-06-19');
 
 insert into dept_locations values (1, 'Houston'),
-								 (4, 'Stafford'),
-                                 (5, 'Bellaire'),
-                                 (5, 'Sugarland'),
-                                 (5, 'Houston');
+    (4, 'Stafford'),
+    (5, 'Bellaire'),
+    (5, 'Sugarland'),
+    (5, 'Houston');
 
 insert into project values ('ProductX', 1, 'Bellaire', 5),
-						   ('ProductY', 2, 'Sugarland', 5),
-						   ('ProductZ', 3, 'Houston', 5),
-                           ('Computerization', 10, 'Stafford', 4),
-                           ('Reorganization', 20, 'Houston', 1),
-                           ('Newbenefits', 30, 'Stafford', 4)
-;
+    ('ProductY', 2, 'Sugarland', 5),
+    ('ProductZ', 3, 'Houston', 5),
+    ('Computerization', 10, 'Stafford', 4),
+    ('Reorganization', 20, 'Houston', 1),
+    ('Newbenefits', 30, 'Stafford', 4);
 
 insert into works_on values (123456789, 1, 32.5),
-							(123456789, 2, 7.5),
-                            (666884444, 3, 40.0),
-                            (453453453, 1, 20.0),
-                            (453453453, 2, 20.0),
-                            (333445555, 2, 10.0),
-                            (333445555, 3, 10.0),
-                            (333445555, 10, 10.0),
-                            (333445555, 20, 10.0),
-                            (999887777, 30, 30.0),
-                            (999887777, 10, 10.0),
-                            (987987987, 10, 35.0),
-                            (987987987, 30, 5.0),
-                            (987654321, 30, 20.0),
-                            (987654321, 20, 15.0),
-                            (888665555, 20, 0.0);
+    (123456789, 2, 7.5),
+    (666884444, 3, 40.0),
+    (453453453, 1, 20.0),
+    (453453453, 2, 20.0),
+    (333445555, 2, 10.0),
+    (333445555, 3, 10.0),
+    (333445555, 10, 10.0),
+    (333445555, 20, 10.0),
+    (999887777, 30, 30.0),
+    (999887777, 10, 10.0),
+    (987987987, 10, 35.0),
+    (987987987, 30, 5.0),
+    (987654321, 30, 20.0),
+    (987654321, 20, 15.0),
+    (888665555, 20, 0.0);
 
 -- Consultas SQL
 
 select * from employee;
-select Ssn, count(Essn) from employee e, dependent d where (e.Ssn = d.Essn);
 select * from dependent;
+
+-- select Ssn, count(Essn) from employee e, dependent d where (e.Ssn = d.Essn);
+
+-- https://stackoverflow.com/questions/23921117/disable-only-full-group-by
+-- Solution 1: Remove ONLY_FULL_GROUP_BY from mysql console
+-- mysql > SET GLOBAL sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+-- Be aware that this setting is NOT persistent across restarts, then use
+-- mysql > SET PERSIST sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''));
+-- or use like this
+
+select Ssn, count(Essn) from employee e, dependent d where (e.Ssn = d.Essn) GROUP BY e.Ssn;
+
 
 SELECT Bdate, Address FROM employee
 WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
@@ -104,11 +114,11 @@ SELECT * FROM employee WHERE Dno IN (3,6,9);
 
 SELECT Bdate, Address
 FROM EMPLOYEE
-WHERE Fname = ‘John’ AND Minit = ‘B’ AND Lname = ‘Smith’;
+WHERE Fname = 'John' AND Minit = 'B' AND Lname = 'Smith';
 
 SELECT Fname, Lname, Address
-FROM EMPLOYEE, DEPARTMENT
-WHERE Dname = ‘Research’ AND Dnumber = Dno;
+FROM EMPLOYEE, DEPARTAMENT
+WHERE Dname = 'Research' AND Dnumber = Dno;
 
 --
 --
--- powerbi tasks/azure database/script_bd_company.sql
+++ powerbi tasks/azure database/script_bd_company.sql
@@ -5,13 +5,13 @@ create schema if not exists azure_company;
 use azure_company;
 
 select * from information_schema.table_constraints
-	where constraint_schema = 'azure_company';
+    where constraint_schema = 'azure_company';
 
 -- restrição atribuida a um domínio
 -- create domain D_num as int check(D_num> 0 and D_num< 21);
 
 CREATE TABLE employee(
-	Fname varchar(15) not null,
+    Fname varchar(15) not null,
     Minit char,
     Lname varchar(15) not null,
     Ssn char(9) not null, 
@@ -26,8 +26,8 @@ CREATE TABLE employee(
 );
 
 alter table employee 
-	add constraint fk_employee 
-	foreign key(Super_ssn) references employee(Ssn)
+    add constraint fk_employee 
+    foreign key(Super_ssn) references employee(Ssn)
     on delete set null
     on update cascade;
 
@@ -36,7 +36,7 @@ alter table employee modify Dno int not null default 1;
 desc employee;
 
 create table departament(
-	Dname varchar(15) not null,
+    Dname varchar(15) not null,
     Dnumber int not null,
     Mgr_ssn char(9) not null,
     Mgr_start_date date, 
@@ -51,28 +51,29 @@ create table departament(
 -- modificar uma constraint: drop e add
 alter table departament drop  departament_ibfk_1;
 alter table departament 
-		add constraint fk_dept foreign key(Mgr_ssn) references employee(Ssn)
-        on update cascade;
+    add constraint fk_dept foreign key(Mgr_ssn) references employee(Ssn)
+    on update cascade;
 
 desc departament;
 
 create table dept_locations(
-	Dnumber int not null,
-	Dlocation varchar(15) not null,
+    Dnumber int not null,
+    Dlocation varchar(15) not null,
     constraint pk_dept_locations primary key (Dnumber, Dlocation),
     constraint fk_dept_locations foreign key (Dnumber) references departament (Dnumber)
 );
 
-alter table dept_locations drop fk_dept_locations;
+-- alter table dept_locations drop fk_dept_locations;
 
-alter table dept_locations 
-	add constraint fk_dept_locations foreign key (Dnumber) references departament(Dnumber)
-	on delete cascade
+/*alter table dept_locations 
+    add constraint fk_dept_locations foreign key (Dnumber) references departament(Dnumber)
+    on delete cascade
     on update cascade;
+*/
 
 create table project(
-	Pname varchar(15) not null,
-	Pnumber int not null,
+    Pname varchar(15) not null,
+    Pnumber int not null,
     Plocation varchar(15),
     Dnum int not null,
     primary key (Pnumber),
@@ -82,7 +83,7 @@ create table project(
 
 
 create table works_on(
-	Essn char(9) not null,
+    Essn char(9) not null,
     Pno int not null,
     Hours decimal(3,1) not null,
     primary key (Essn, Pno),
@@ -92,7 +93,7 @@ create table works_on(
 
 drop table dependent;
 create table dependent(
-	Essn char(9) not null,
+    Essn char(9) not null,
     Dependent_name varchar(15) not null,
     Sex char,
     Bdate date,
